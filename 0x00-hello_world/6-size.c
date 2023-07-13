2ea7ef656f1d5a9f19d5db379fbe88b6f06d818b
Yeabsira Yonas <121499744+yabulala432@users.noreply.github.com>
2023-07-13 16:05:38 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,10 @@
  */
 int main(void)
 {
-printf("Size of char is: %ud \n" , sizeof(char));
-printf("Size of int is: %ud \n" , sizeof(int));
-printf("Size of short is: %ud\n" , sizeof(short));
-printf("Size of long is: %ud \n" , sizeof(long));
-printf("Size of float is:%ud \n" , sizeof(float));
-printf("Size of double is: %ud \n" , sizeof(double));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
