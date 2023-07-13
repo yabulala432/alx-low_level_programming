b32fb730768ff5d776bf02cbb566092943163c62
yabulala432 <yabulala432@gmail.com>
2023-07-13 05:53:43 -0700

    commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of char is: " ,sizeof(char),"\n")
+printf("Size of int is: " ,sizeof(int),"\n")
+printf("Size of short is: " ,sizeof(short),"\n")
+printf("Size of long is: " ,sizeof(long),"\n")
+printf("Size of float is: " ,sizeof(float),"\n")
+printf("Size of double is: " ,sizeof(double),"\n")
+return (0);
+}
