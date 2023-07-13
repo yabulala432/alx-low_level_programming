029e00ba87100958bf665d50c01e97083c20e9de
yabulala432 <yabulala432@gmail.com>
2023-07-13 06:03:39 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-printf("Size of char is: %ud \n" ,sizeof(char));
-printf("Size of int is: %ud \n" ,sizeof(int));
-printf("Size of short is: %ud\n" ,sizeof(short));
-printf("Size of long is: %ud \n" ,sizeof(long));
-printf("Size of float is:%ud \n" ,sizeof(float));
-printf("Size of double is: %ud \n" ,sizeof(double));
+printf("Size of char is: %ud \n" , sizeof(char));
+printf("Size of int is: %ud \n" , sizeof(int));
+printf("Size of short is: %ud\n" , sizeof(short));
+printf("Size of long is: %ud \n" , sizeof(long));
+printf("Size of float is:%ud \n" , sizeof(float));
+printf("Size of double is: %ud \n" , sizeof(double));
 return (0);
 }
